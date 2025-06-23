190baa9723fcb80bf7fe26537b4faefeec395a53
BellaAkins <sarabellaakins@gmail.com>
2025-06-23 01:26:48 +0100

    i added hero section

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("open-nav");
+});
+close.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
+});
+
+//To close the navbar when a link is clicked
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.getElementById("navbar");
+  const navLinks = navbar.querySelectorAll("a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      //  navbar.style.transform = "translateX(100%)";
+      nav.classList.remove("open-nav");
+    });
+  });
+});
+
 const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach((faq) => {
